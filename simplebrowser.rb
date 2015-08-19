395421e5af1e75d3d434317c739043f4093aa766
Jack <Jack7100@gmail.com>
2015-08-19 16:05:49 -0400

    Finished project!

--- simplebrowser.rb
+++ simplebrowser.rb
@@ -1,11 +1,44 @@
 require 'socket'
+require 'json'
 
 hostname = 'localhost'
 port = 2000
 
-s = TCPSocket.open(hostname, port)
+puts "Welcome to the Simple Web Browser!"
+print "What do you want to do? (GET or POST) "
+input = gets.chomp.upcase
 
-while line = s.gets #Read lines from socket
-  puts line.chop  #print with platform line terminator
-end
-s.close #Close socket when done
+  def thanks_data
+    puts "Form to register for a Viking Raid. "
+    puts "Please enter a name"
+    name = gets.chomp
+    puts "Now please enter an email"
+    email = gets.chomp
+    hash_form = {:viking => {:name => name, :email => email}}
+    data = hash_form.to_json
+    puts data
+    return data
+  end
+
+      if input == "GET"
+        path = './index.html'
+        request = "GET #{path} HTTP/1.0\r\n\r\n"
+      elsif input == "POST"
+        submit_data = thanks_data
+        puts submit_data
+        request = "POST ./thanks.html HTTP/1.0\nContent-Type: JSON\nContent-Length: #{submit_data.size}\r\n\r\n#{submit_data}"
+      else
+        puts "Invalid input, please try again"
+    end
+
+#while line = s.gets #Read lines from socket
+#  puts line.chop  #print with platform line terminator
+#end
+
+socket = TCPSocket.open(hostname, port)
+socket.print(request)
+response = socket.read
+
+header, body = response.split("\r\n\r\n", 2)
+print body
+socket.close #Close socket when done
--- simpleserver.rb
+++ simpleserver.rb
@@ -1,11 +1,49 @@
 require 'socket'
+require 'json'
 
 server = TCPServer.open(2000)  #Socket to listen on port 2000
 
 loop {  # Server runs forever
   client = server.accept  #Wait for client to connect
-  
+  header_lines = ""
+  while line = client.gets
+		header_lines += line
+		break if header_lines =~ /\r\n\r\n$/
+	end
+
+	header = header_lines.split("\n")
+	request = header[0].split(" ")
+
+	method = request[0]
+	filepath = request[1]
+  http = request[2]
+
+	puts method
+	puts filepath
+  puts http
+
+  if File.exists?(filepath)
+
+    file = File.read(filepath)
+    client.print("#{http} 200 OK\r\nContent-Type: text/html\r\n Content-Length: #{File.size(filepath)}\r\n\r\n")
+      if method == "GET"
+        client.puts(file)
+      elsif method == "POST"
+        readBody = header[-2].split(" ")[1].to_i
+        body = client.read(readBody)
+        params = JSON.parse(body)
+        puts params
+        puts params.keys
+        data = "<li>Name: #{params['viking']['name']}</li><li>Email: #{params['viking']['email']}</li>"
+        puts data
+        file = file.gsub("<%= yield %>", data)
+        client.puts "#{http} 200 OK\nContent-Type: html\nContent-Length: #{file.size}\r\n\r\n#{file}"
+      end
+  else
+    client.puts("HTTP/1.0 404 Not Found")
+  end
+
   #client.puts(Time.now.ctime) #Send time to client
   #client.puts "Closing the connection. Bye!" #Sends message to client
-  #client.close #Closes connection. Disconnect from the client
+  client.close #Closes connection. Disconnect from the client
 }
